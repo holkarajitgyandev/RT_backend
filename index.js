a418e8c619465d086b588378009365403e800eac
holkarajitgyandev <139675885+holkarajitgyandev@users.noreply.github.com>
2025-02-10 11:18:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,8 +14,13 @@ dotenv.config();
 const app= express();
 //middleware
 app.use(express.json());
+const corsOptions = {
+  origin: "http://localhost:5173", // Allow requests from this origin
+  methods: ["GET", "POST"], // Specify allowed methods
+  credentials: true, // Allow cookies or credentials
+};
 
-app.use(cors())
+app.use(cors(corsOptions))
 app.use(cookieParser());
 
 
